de7ad1f692384cd3cb1844c4584c5b8895385d7a
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2023-01-17 15:19:48 +0100

    feat: added new function 27/71

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -219,8 +219,8 @@ function isPrime(num) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) || def;
 }
 
 module.exports = {
