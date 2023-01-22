e3c040f71323b4d8a83424f7347ccf1f340970d3
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2023-01-22 21:36:06 +0100

    feat: update 58/98

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -587,8 +587,11 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const mid = Math.floor(arr.length / 2);
+  const head = arr.splice(0, mid);
+  const tail = arr.splice(-mid);
+  return tail.concat(arr, head);
 }
 
 module.exports = {
